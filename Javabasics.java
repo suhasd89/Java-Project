a85c33c94e3d8865bdfe85f1e23f2fa5fcd9be93
suhasd89 <suhasdeshmukh550@gmail.com>
2021-02-16 18:18:14 +0530

    added print for Feature1

--- Javabasics.java
+++ Javabasics.java
@@ -2,5 +2,6 @@ public class Javabasics{
     public static void main(String[] args) {
         System.out.println("this is main class");
         System.out.println("javabasics class");
+        System.out.println("javabasics for feature branch");
     }
 }
