1db87342dd9fa50f87d2f980a9d80077388c8633
suhasd89 <suhasdeshmukh550@gmail.com>
2021-02-16 17:46:32 +0530

    basics commited

--- Javabasics.java
+++ Javabasics.java
@@ -1,6 +1,6 @@
 public class Javabasics{
     public static void main(String[] args) {
         System.out.println("this is main class");
-        System.out.println("for github");
+        System.out.println("javabasics class");
     }
 }
